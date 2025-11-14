15ff9ca47d02db8e83c6a7c59a712364e1d0502a
Alan raja zulkifli <alanrajazulkifli@gmail.com>
2025-11-14 11:35:34 +0700

    alan

--- alan/user.php
+++ alan/user.php
@@ -2,7 +2,7 @@
    $data = "localhost";
    $login   = "root";
    $password   = "rpl12345";
-   $database   = "db_login";
+   $database   = "db_datasiswa";
    $conn = new mysqli($data, $login, $password, $database);
    if ($conn->connect_error) {
     die("Koneksi gagal: " . $conn->connect_error);
