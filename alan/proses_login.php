9f3febb1e3192ba154fda3568969e33aaeab66cd
Alan raja zulkifli <alanrajazulkifli@gmail.com>
2025-11-14 13:04:30 +0700

    alan

--- alan/proses_login.php
+++ alan/proses_login.php
@@ -1,11 +1,34 @@
-<!DOCTYPE html>
-<html lang="en">
-<head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Document</title>
-</head>
-<body>
-    <h1>hai</h1>
-</body>
-</html>
+
+<?php
+
+session_start();
+  include "koneksi.php";
+ $username = $_POST['username'];
+ $password = $_POST['password'];
+
+$stmt = $conn->prepare("SELECT * FROM user WHERE username = ? LIMIT 1");
+$stmt->bind_param("s", $username);
+$stmt->execute();
+$result = $stmt->get_result();
+
+if ($result->num_rows > 0) {
+    $data = $result->fetch_assoc();
+
+    // Verifikasi password
+    if (password_verify($password, $data['password'])) {
+        $_SESSION['login'] = true;
+        $_SESSION['username'] = $data['username'];
+        header("Location: dashboard.php");
+        exit;
+    } else {
+        $_SESSION['error'] = "Password salah!";
+        header("Location: login.php");
+        exit;
+    }
+    } else {
+    $_SESSION['error'] = "Username tidak ditemukan!";
+    header("Location: login.php");
+    exit;
+    }
+
+    ?>
