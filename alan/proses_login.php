762950063ef3208035c89e1014eec0be8432ab4b
Alan raja zulkifli <alanrajazulkifli@gmail.com>
2025-11-17 16:12:43 +0700

    alan

--- alan/proses_login.php
+++ alan/proses_login.php
@@ -1,7 +1,8 @@
 <?php
 session_start();
 include "koneksi.php";
-
+$username = $_POST['username'];
+ $password = $_POST['password'];
 
 if ($conn->connect_error) {
     $_SESSION['error'] = "Koneksi database gagal: " . $conn->connect_error;
@@ -9,7 +10,6 @@ if ($conn->connect_error) {
     exit;
 }
 
-// 2. Perbaikan sintaks array $_POST dan validasi dasar
 if (isset($_POST['username']) && isset($_POST['password'])) {
     $uname = $_POST['username'];
     $password = $_POST['password'];
@@ -19,19 +19,16 @@ if (isset($_POST['username']) && isset($_POST['password'])) {
     exit;
 }
 
-// Query menggunakan Prepared Statement (Sangat Baik!)
 $stmt = $conn->prepare("SELECT * FROM user WHERE username = ? LIMIT 1");
 
-// 3. Perbaikan bind_param: Hanya mengikat $uname (satu string)
 $stmt->bind_param("s", $uname);
 $stmt->execute();
 $result = $stmt->get_result();
-$stmt->close(); // Tutup statement setelah selesai digunakan
+$stmt->close();
 
 if ($result->num_rows > 0) {
     $data = $result->fetch_assoc();
 
-    // Verifikasi password (menggunakan password_verify sudah BENAR!)
     if (password_verify($password, $data['password'])) {
         $_SESSION['login'] = true;
         $_SESSION['username'] = $data['username'];
