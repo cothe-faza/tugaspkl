2bd08748a3be8bcf2d488f1283941e393f0bb306
sitikholisah <olis.kholisah14@gmail.com>
2025-11-25 12:20:03 +0700

    proses-login.php

---
+++ kholis/tugas-lima.php
@@ -0,0 +1,8 @@
+$servername = "localhost";
+$username   = "root";
+$password   = "";
+$database   = "db_login";
+$conn = new mysqli($servername, $username, $password, $database);
+if ($conn->connect_error) {
+ die("Koneksi gagal: " . $conn->connect_error);
+}
