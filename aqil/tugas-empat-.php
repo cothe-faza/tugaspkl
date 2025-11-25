ceaa5abdb2e5c3e4e1df9f9c0a20766a78687b5b
aqilaisir-pixel <aqilaisir@gmail.com>
2025-11-25 13:22:38 +0700

    aqil

--- aqil/tugas-empat-.php
+++ aqil/tugas-empat-.php
@@ -1,16 +1,23 @@
-<tbody>
 <?php
-$no = 1;
-foreach ($result as $data) {
+include"koneksi.php";
+if(isset($_POST['btn'])){
+    $a = $_POST['nis'];
+    $b = $_POST['nama'];
+    $c = $_POST['kelas'];
+     $qry = $conn->query("INSERT INTO tb_siswa(nis,nama,kelas) VALUES ('$a','$b','$c')");// silahkan kalian buat query di titik-titik..
+if($qry == true){
+     echo"<script>alert('Data Berhasil diinput....')</script>";
+}else{
+     echo"<script>alert('Data gagal diinput....')</script>";
+    }      
+}
 ?>
-<tr>
-    <th scope="row"><?= $no++; ?></th>
-    <td><?= $data['nis']; ?></td>
-    <td><?= $data['nama']; ?></td>
-    <td><?= $data['kelas']; ?></td>
-</tr>
-<?php } ?>
-</tbody>
-<?php
-     $conn = new mysqli('localhost','root','rpl12345','db_datasiswa'); // untuk password sesuiakan dengan localserver masing-masing
-?>
+<form method="post">
+    <label>NIS</label>
+    <input type="text" name="nis">
+    <label>Nama</label>
+    <input type="text" name="nama">
+    <label>Kelas</label>
+    <input type="text" name="kelas">
+    <button type="submit" name="btn">Submit</button>
+</form>
