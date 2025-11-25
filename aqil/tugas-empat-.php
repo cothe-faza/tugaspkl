f2809f25696b36c71d7ab10f5e42c586ce687b6b
aqilaisir-pixel <aqilaisir@gmail.com>
2025-11-25 13:16:31 +0700

    aqil

--- aqil/tugas-empat-.php
+++ aqil/tugas-empat-.php
@@ -12,24 +12,5 @@ foreach ($result as $data) {
 <?php } ?>
 </tbody>
 <?php
-include "koneksi.php";
-
-if (isset($_POST['btn'])) {
-
-    $a = $_POST['nis'];
-    $b = $_POST['nama'];
-    $c = $_POST['kelas'];
-
-    // Query insert
-    $qry = $conn->query("
-        INSERT INTO tb_siswa (nis, nama, kelas)
-        VALUES ('$a', '$b', '$c')
-    ");
-
-    if ($qry == true) {
-        echo "<script>alert('Data Berhasil diinput....')</script>";
-    } else {
-        echo "<script>alert('Data gagal diinput....')</script>";
-    }
-}
+     $conn = new mysqli('localhost','root','rpl12345','db_datasiswa'); // untuk password sesuiakan dengan localserver masing-masing
 ?>
