00a6c38e8f16609956cee118d23085f12e13ca5d
walludin537 <uddinsyawal943@gmail.com>
2025-11-25 13:07:28 +0700

    sawal

---
+++ sawal/oop-pdo-crud-siswa/│── proses/hapus_proses.php
@@ -0,0 +1,13 @@
+<?php
+include '../config/Database.php';
+include '../classes/Siswa.php';
+
+$db       = new Database();
+$conn     = $db->connect();
+$siswaObj = new Siswa($conn);
+
+$siswaObj->id = $_GET['id'];
+$siswaObj->delete();
+
+header("Location: ../views/index.php");
+?>
