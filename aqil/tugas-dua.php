9a4dcea5a1d6e5bb6021f474f9c627f24aa69ae4
aqilaisir-pixel <aqilaisir@gmail.com>
2025-11-25 12:42:48 +0700

    aqil/tugas-dua.php

--- aqil/tugas-dua.php
+++ aqil/tugas-dua.php
@@ -1,5 +1,22 @@
-CREATE TABLE tb_siswa (
-    nis VARCHAR(20) PRIMARY KEY,
-    nama VARCHAR(50),
-    kelas VARCHAR(20)
-);localhost/phpmyadmin
+<?php
+include "koneksi.php";
+
+if (isset($_POST['btn'])) {
+
+    $a = $_POST['nis'];
+    $b = $_POST['nama'];
+    $c = $_POST['kelas'];
+
+    // Query insert
+    $qry = $conn->query("
+        INSERT INTO tb_siswa (nis, nama, kelas)
+        VALUES ('$a', '$b', '$c')
+    ");
+
+    if ($qry == true) {
+        echo "<script>alert('Data Berhasil diinput....')</script>";
+    } else {
+        echo "<script>alert('Data gagal diinput....')</script>";
+    }
+}
+?>
