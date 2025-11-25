be08d431795b2951ed91359d468656909cafb244
aqilaisir-pixel <aqilaisir@gmail.com>
2025-11-25 21:01:10 +0700

    aqil

--- aqil/tugas-lima.php
+++ aqil/tugas-lima.php
@@ -1,27 +1,42 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title></title>
+</head>
+<body>
+    
+
+
 <?php
      include "koneksi.php";
    if(isset($_POST['btn'])){
-       $a = $_POST['nis'];
+       $a = $_POST['kode'];
        $b = $_POST['nama'];
-       $c = $_POST['kelas'];
-        $qry = $conn->query("INSERT INTO tb_siswa(nis,nama,kelas) VALUES ('$a','$b','$c')");
-        // silahkan kalian buat query di titik-titik..
-   if($qry == true){
+       $c = $_POST['harga'];
+       $d = $_POST['jumlah'];
+       $e = $_POST['kondisi'];
+        $qry = $conn->query("INSERT INTO tb_barang(kd_barang, nama_brg, harga_brg, jumlah_brg, kondisi_brg) VALUES ('$a','$b','$c','$d','$e')");
+    if($qry == true){
         echo"<script>alert('Data Berhasil diinput....')</script>";
    }else{
         echo"<script>alert('Data gagal diinput....')</script>";
      }
    }
-
-   ?>
+   ?> 
  
  <form method="post">       
-    <label>NIS</label>
-    <input type="text" name="nis">
-    <label>Nama</label>
-    <input type="text" name="nama">
-    <label>Kelas</label>
-    <input type="text" name="kelas">
+    <label>kode barang</label>
+    <input type="number" name="kode"><br>
+    <label>Nama barang</label>
+    <input type="text" name="nama"><br>
+    <label>harga barang</label>
+    <input type="number" name="harga"><br>
+    <label>jumlah barang</label>
+    <input type="number" name="jumlah"><br>
+    <label>kondisi barang</label>
+    <input type="text" name="kondisi"><br>
     <button type="submit" name="btn">Submit</button>
 </form>
 
@@ -30,22 +45,32 @@
        <thead>
        <tr>
        <th scope="col">No</th>
-       <th scope="col">NIS</th>
-       <th scope="col">Nama</th>
-       <th scope="col">Kelas</th>
+       <th scope="col">kode barang</th>
+       <th scope="col">nama barang</th>
+       <th scope="col">harga barang</th>
+       <th scope="col">jumlah barang</th>
+       <th scope="col">kondisi barang</th>
+       <th>Aksi</th>
+
        </tr>
        </thead>
        <tbody>
           <?php
           $no = 1;
-          $sqlResult = $conn->query("SELECT*FROM tb_siswa");
+          $sqlResult = $conn->query("SELECT*FROM tb_barang");
           foreach($sqlResult as $data){
             ?>
        <tr>
        <td><?=$no++?></td>
-       <td><?=$data['nis']?></td>
-       <td><?=$data['nama']?></td>
-       <td><?=$data['kelas']?></td>
+       <td><?=$data['kd_barang']?></td>
+       <td><?=$data['nama_brg']?></td>
+       <td>Rp.   <?=number_format($data['harga_brg'])?></td>
+       <td><?=$data['jumlah_brg']?></td>
+       <td><?=$data['kondisi_brg']?></td>
+       <td>
+            <a href="#" class="btn btn-warning btn-sm">Edit</a>
+            <a href="#" class="btn btn-danger btn-sm">Hapus</a>
+        </td>
        </tr>
        
     
@@ -56,3 +81,5 @@
  </tbody>
        </table>
       
+    </body>
+</html>
