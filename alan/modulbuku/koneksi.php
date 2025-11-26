a4fbb03358439df2b2b19339e3fff1044877d3c1
Alan raja zulkifli <alanrajazulkifli@gmail.com>
2025-11-26 13:57:16 +0700

    a

--- alan/modulbuku/koneksi.php
+++ alan/modulbuku/koneksi.php
@@ -1,4 +1,10 @@
  <?php
-        $conn = new mysqli('localhost','root','rpl12345','db_perpustakaan');
+        $conn = new mysqli('loalhost','root','rpl12345','db_perpustakaan');
          // untuk password sesuiakan dengan localserver masing-masing
+         if ($conn) {
+            echo"Done";
+         } else {
+            echo"Mampus";
+         }
+         
    ?>
