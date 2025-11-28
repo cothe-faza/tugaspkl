3bd0a2cb83b23045c387539d416674d9f0e9af23
naysilaa-jpg <naysil4nay@gmail.com>
2025-11-28 13:12:08 +0700

    tugas

--- naysila/koneksi.php
+++ naysila/koneksi.php
@@ -1,3 +1,3 @@
 <?php
-     $conn = new mysqli('localhost','root','rpl12345','db_datasiswanay'); // untuk password sesuiakan dengan localserver masing-masing
+     $conn = new mysqli('localhost','root','rpl12345','db_perputakaannay'); // untuk password sesuiakan dengan localserver masing-masing
 ?>
